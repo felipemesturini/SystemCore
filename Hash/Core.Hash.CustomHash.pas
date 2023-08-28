9650a4d4e0cf63aea56be9845c0446a68f221131
MERCURIO-BOOK\felip <felipe_mesturini@outlook.com>
2023-08-28 15:13:45 -0300

    HAs

--- Hash/Core.Hash.CustomHash.pas
+++ Hash/Core.Hash.CustomHash.pas
@@ -5,8 +5,12 @@ interface
 type
   TCustomHash = record
   public
-    class function EncodeValue(const AValue: String; const AKey: String = '12dc56de5007d4569efe558a83a3b0c1'): String; static;
-    class function DecodeValue(const AValue: String; const AKey: String = '12dc56de5007d4569efe558a83a3b0c1'): String; static;
+    class function EncodeValue(
+      const AValue: string;
+      const AKey: string = '12dc56de5007d4569efe558a83a3b0c1'): string; static;
+    class function DecodeValue(
+      const AValue: string;
+      const AKey: string = '12dc56de5007d4569efe558a83a3b0c1'): string; static;
   end;
 
 implementation
@@ -14,45 +18,49 @@ implementation
 uses
   System.Math,
   System.Hash,
-  JvCipher, System.SysUtils;
+  JvCipher,
+  System.SysUtils;
 
 { TCustomHash }
 
-class function TCustomHash.DecodeValue(const AValue: String; const AKey: String = '12dc56de5007d4569efe558a83a3b0c1'): String;
+class function TCustomHash.DecodeValue(
+  const AValue: string;
+  const AKey: string = '12dc56de5007d4569efe558a83a3b0c1'): string;
 var
-  lCipher: TJvVigenereCipher;
+  i: Byte;
+  StartKey, MultKey, AddKey: Word;
 begin
   Result := '';
-  if (AKey = '') then
-    raise EInvalidArgument.Create('AKey must have a value');
-  lCipher := TJvVigenereCipher.Create(nil);
-  try
-    lCipher.Key := AnsiString(AKey);
-    lCipher.Encoded := AnsiString(AValue);
-    Result := String(lCipher.Decoded);
-  finally
-    FreeAndNil(lCipher);
+  StartKey := Length(AKey);
+  MultKey := Ord(AKey[1]);
+  AddKey := 0;
+  for i := 1 to Length(AKey) - 1 do
+    AddKey := AddKey + Ord(AKey[i]);
+  for i := 1 to Length(AValue) do
+  begin
+    Result := Result + CHAR(Byte(AValue[i]) xor (StartKey shr 8));
+    StartKey := (Byte(Result[i]) + StartKey) * MultKey + AddKey;
   end;
-
 end;
 
-class function TCustomHash.EncodeValue(const AValue: String; const AKey: String = '12dc56de5007d4569efe558a83a3b0c1'): String;
+class function TCustomHash.EncodeValue(
+  const AValue: string;
+  const AKey: string = '12dc56de5007d4569efe558a83a3b0c1'): string;
 var
-  lCipher: TJvVigenereCipher;
+  i: Byte;
+  StartKey, MultKey, AddKey: Word;
 begin
   Result := '';
-  if (AKey = '') then
-    raise EInvalidArgument.Create('AKey must have a value');
-
-  lCipher := TJvVigenereCipher.Create(nil);
-  try
-    lCipher.Key := AnsiString(AKey);
-    lCipher.Decoded := AnsiString(AValue);
-    Result := String(lCipher.Encoded);
-  finally
-    FreeAndNil(lCipher);
+  StartKey := Ord(AKey[1]);
+  MultKey := Length(AKey);
+  AddKey := 0;
+  for i := 1 to Length(AKey) - 1 do
+    AddKey := AddKey + Ord(AKey[i]);
+  for i := 1 to Length(AValue) do
+  begin
+    Result := Result + CHAR(Byte(AValue[i]) xor (StartKey shr 8));
+    StartKey := (Byte(AValue[i]) + StartKey) * MultKey + AddKey;
   end;
-
 end;
 
 end.
