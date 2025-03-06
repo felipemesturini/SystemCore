0bb90d6bdf284c9febf6647ca42d0fc56254395d
MERCURIO-BOOK\felip <felipe_mesturini@outlook.com>
2025-03-06 13:36:25 -0300

    Inclusão de funão de data

--- DataHora/System.DataHora.DateUtils.pas
+++ DataHora/System.DataHora.DateUtils.pas
@@ -13,6 +13,7 @@ type
     class function LastDateOfPriorWeek: TDate;
     class function FirsDateOfPriorMonth: TDate;
     class function LastDateOfPriorMonth: TDate;
+    class function MonthsBetweenDates(AStartDate, AEndDate: TDate): Word;
   end;
 
 implementation
@@ -25,15 +26,31 @@ uses
 
 class function TDateUtils.FirsDateOfMonth: TDate;
 begin
-  Result := EncodeDateTime(YearOf(Now), MonthOf(Now), 1, 0, 0, 0, 0);
+  Result := EncodeDateTime(
+    YearOf(Now),
+    MonthOf(Now),
+    1,
+    0,
+    0,
+    0,
+    0);
 end;
 
 class function TDateUtils.FirsDateOfPriorMonth: TDate;
 var
   lData: TDate;
 begin
-  lData := IncMonth(Now, -1);
-  Result := EncodeDateTime(YearOf(lData), MonthOf(lData), 1, 0, 0, 0, 0);
+  lData := IncMonth(
+    Now,
+    -1);
+  Result := EncodeDateTime(
+    YearOf(lData),
+    MonthOf(lData),
+    1,
+    0,
+    0,
+    0,
+    0);
 end;
 
 class function TDateUtils.FirstDateOfPriorWeek: TDate;
@@ -51,15 +68,31 @@ end;
 
 class function TDateUtils.LastDateOfMonth: TDate;
 begin
-  Result := EncodeDateTime(YearOf(Now), MonthOf(Now), DaysInMonth(Now), 0, 0, 0, 0);
+  Result := EncodeDateTime(
+    YearOf(Now),
+    MonthOf(Now),
+    DaysInMonth(Now),
+    0,
+    0,
+    0,
+    0);
 end;
 
 class function TDateUtils.LastDateOfPriorMonth: TDate;
 var
   lData: TDate;
 begin
-  lData := IncMonth(Now, -1);
-  Result := EncodeDateTime(YearOf(lData), MonthOf(lData), DaysInMonth(lData), 0, 0, 0, 0);
+  lData := IncMonth(
+    Now,
+    -1);
+  Result := EncodeDateTime(
+    YearOf(lData),
+    MonthOf(lData),
+    DaysInMonth(lData),
+    0,
+    0,
+    0,
+    0);
 end;
 
 class function TDateUtils.LastDateOfPriorWeek: TDate;
@@ -73,6 +106,38 @@ var
 begin
   lData := EndOfTheWeek(Now);
   Result := lData;
+end;
+
+class function TDateUtils.MonthsBetweenDates(AStartDate, AEndDate: TDate): Word;
+var
+  lStartYear: Word;
+  lStartMonth: Word;
+  lStartDay: Word;
+  lEndYear: Word;
+  lEndMonth: Word;
+  lEndDay: Word;
+begin
+  DecodeDate(
+    AStartDate,
+    lStartYear,
+    lStartMonth,
+    lStartDay);
+  DecodeDate(
+    AEndDate,
+    lEndYear,
+    lEndMonth,
+    lEndDay);
+
+  Result := (lEndYear - lStartYear) * 12 + (lEndMonth - lStartMonth);
+
+  // Adjust for cases where the end date's day is before the start date's day.
+  if (lEndYear = lStartYear) and (lEndMonth = lStartMonth) and (lEndDay < lStartDay) then
+    Result := 0;
+//  else if (lEndDay < lStartDay) then
+//    Dec(Result);
+
+  if Result < 0 then
+    Result := 0
 end;
 
 end.
