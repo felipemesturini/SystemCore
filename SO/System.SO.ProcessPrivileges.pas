786fbe0dac32a71f6822e6274b2d23d20a68073b
MERCURIO-BOOK\felip <felipe_mesturini@outlook.com>
2025-12-26 17:17:55 -0300

    Privilegios

--- SO/System.SO.ProcessPrivileges.pas
+++ SO/System.SO.ProcessPrivileges.pas
@@ -4,8 +4,7 @@ interface
 
 uses
   Winapi.ShellAPI,
-  Winapi.Windows,
-  JclSecurity;
+  Winapi.Windows;
 
 type
   TProcessPrivileges = class sealed
@@ -13,16 +12,39 @@ type
     class function HasAdminAccess(): Boolean;
   end;
 
-//function IsUserAnAdmin(): BOOL; external shell32;
+  // function IsUserAnAdmin(): BOOL; external shell32;
 
 implementation
 
 { TProcessPrivileges }
 
 class function TProcessPrivileges.HasAdminAccess: Boolean;
+var
+  lProcess: THandle;
+  lToken: THandle;
+  lTokenInformation: TTokenElevation;
+  lPointerTokenInformation: Pointer;
+  lReturnLength: DWORD;
 begin
-  Result := IsElevated();
+  Result := False;
+  lProcess := GetCurrentProcess;
 
+  try
+    if OpenProcessToken(lProcess, TOKEN_QUERY, lToken) then
+      try
+        FillChar(lTokenInformation, SizeOf(lTokenInformation), 0);
+        lPointerTokenInformation := @lTokenInformation;
+        GetTokenInformation(lToken, TokenElevation,
+          lPointerTokenInformation, SizeOf(lTokenInformation),
+          lReturnLength);
+        Result := (lTokenInformation.TokenIsElevated <> 0);
+      finally
+        CloseHandle(lToken);
+      end;
+
+  except
+
+  end;
 end;
 
 end.
